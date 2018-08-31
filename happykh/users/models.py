4f035e1bcebe724d0f2a45478fa3892b654f8fd2
SofiiaOmelchenko <sofiia.omelchenko@nure.ua>
2018-08-31 17:17:18 +0300

    Custom user model with bug

--- happykh/users/models.py
+++ happykh/users/models.py
@@ -1,3 +1,48 @@
+""" Custom models for user """
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """"
+    Customized manager for customized user model
+    """
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(email, password=password)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    """"
+    Customized user model with email as username and additional fields
+    """
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    age = models.PositiveSmallIntegerField()
+    gender = models.CharField(choices=(('Ж', 'woman'), ('М', 'man')), max_length=2, default='Ж')
+    profile_image = models.ImageField(null=True, blank=True)
+    is_admin = models.BooleanField(default=False)
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
