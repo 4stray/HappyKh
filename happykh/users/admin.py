68d67348b3c22a75a9565d5ce7525359034189d0
SofiiaOmelchenko <sofiia.omelchenko@nure.ua>
2018-09-04 10:24:46 +0300

    fixed user's and admin's fields

--- happykh/users/admin.py
+++ happykh/users/admin.py
@@ -1,21 +1,21 @@
-"""" """
+"""" Customized Admin  """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .models import User
 
+@admin.register(User)
 class UserAdmin(BaseUserAdmin):
     """" Class for displaying custom user model on admin site"""
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ('email', 'is_admin')
-    list_filter = ()
+    list_display = ('email', 'is_superuser')
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Personal info', {'fields': ('first_name', 'last_name', 'age', 'gender')}),
         ('Avatar', {'fields': ('profile_image',)}),
-        ('Permissions', {'fields': ('is_admin',)}),
+        ('Permissions', {'fields': ('is_superuser',)}),
     )
     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
     # overrides get_fieldsets to use this attribute when creating a user.
@@ -27,7 +27,3 @@ class UserAdmin(BaseUserAdmin):
     )
     search_fields = ('email',)
     ordering = ('email',)
-    filter_horizontal = ()
-
-
-admin.site.register(User, UserAdmin)
--- happykh/users/models.py
+++ happykh/users/models.py
@@ -1,37 +1,39 @@
 """ Custom models for user """
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
 
 class UserManager(BaseUserManager):
     """"
     Customized manager for customized user model
     """
-    def create_user(self, email, password=None):
+    def create_user(self, email, password=None, **extra_fields):
         """
         Creates and saves a User with the given email, date of
         birth and password.
         """
         if not email:
             raise ValueError('Users must have an email address')
-
-        user = self.model(email=self.normalize_email(email))
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password):
+    def create_superuser(self, email, password, **extra_fields):
         """
         Creates and saves a superuser with the given email, date of
         birth and password.
         """
-        user = self.create_user(email, password=password)
-        user.is_admin = True
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        user = self.create_user(email, password=password, **extra_fields)
         user.save(using=self._db)
         return user
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     """"
     Customized user model with email as username and additional fields
     """
@@ -39,24 +41,10 @@ class User(AbstractBaseUser):
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
     age = models.PositiveSmallIntegerField(blank=True, null=True,)
-    gender = models.CharField(choices=(('Ж', 'woman'), ('М', 'man')), max_length=2, default='Ж')
+    gender = models.CharField(choices=(('W', 'woman'), ('M', 'man')), max_length=2, default='W')
     profile_image = models.ImageField(null=True, blank=True)
-    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-
-    def has_perm(self, perm, obj=None):
-        "Does the user have a specific permission?"
-        return True
-
-    def has_module_perms(self, app_label):
-        "Does the user have permissions to view the app 'app_label'?"
-        return True
-
-    @property
-    def is_staff(self):
-        "Is the user a member of staff?"
-        # Simplest possible answer: All admins are staff
-        return self.is_admin
