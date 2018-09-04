537b29532ac394b3f7ac96b27e70719c210859ad
Dmitri Roganin <dmitriroganin@Dmitris-MacBook-Pro.local>
2018-09-04 22:45:32 +0300

    added drangorestframework to projact. created view for user to login.

--- happykh/users/models.py
+++ happykh/users/models.py
@@ -52,3 +52,19 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+    def get_full_name(self):
+        '''
+        Returns the first_name plus the last_name, with a space in between.
+        '''
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        if full_name.strip():
+            return full_name.strip()
+        else:
+            return self.email
+
+    def get_short_name(self):
+        '''
+        Returns the short name for the user. (only first_name)
+        '''
+        return self.first_name
--- happykh/users/views.py
+++ happykh/users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from users.models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class UserLogin(APIView):
+    """
+    List all users, or create a new snippet.
+    """
+
+    def post(self, request, format=None):
+        email = request.data.get('user_email')
+        password = request.data.get('user_password')
+
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return Response({'status': False, 'message': "No user with such email."})
+
+        if not user.check_password(password):
+            return Response({'status': False, 'message': "Incorrect password."})
+
+        full_name = user.get_full_name()
+
+        return Response({'status': True, 'full_name': full_name})
