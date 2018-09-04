666854c8bed0fc5f8afbcb6f5f32d9cc4194d752
SofiiaOmelchenko <sofiia.omelchenko@nure.ua>
2018-09-04 16:21:23 +0300

    fixed choices and comments

--- happykh/users/models.py
+++ happykh/users/models.py
@@ -7,10 +7,10 @@ class UserManager(BaseUserManager):
     """"
     Customized manager for customized user model
     """
+
     def create_user(self, email, password=None, **extra_fields):
         """
-        Creates and saves a User with the given email, date of
-        birth and password.
+        Creates and saves a User with the given email and password.
         """
         if not email:
             raise ValueError('Users must have an email address')
@@ -23,8 +23,7 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, email, password, **extra_fields):
         """
-        Creates and saves a superuser with the given email, date of
-        birth and password.
+        Creates and saves a superuser with the given email and password.
         """
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
@@ -37,11 +36,16 @@ class User(AbstractBaseUser, PermissionsMixin):
     """"
     Customized user model with email as username and additional fields
     """
+    man, woman = 'M', 'W'
+    GENDER_CHOICES = (
+        (woman, 'woman'),
+        (man, 'man')
+    )
     email = models.EmailField(max_length=255, unique=True)
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
-    age = models.PositiveSmallIntegerField(blank=True, null=True,)
-    gender = models.CharField(choices=(('W', 'woman'), ('M', 'man')), max_length=2, default='W')
+    age = models.PositiveSmallIntegerField(blank=True, null=True, )
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=2, default=woman)
     profile_image = models.ImageField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
