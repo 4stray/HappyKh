1ff1d6fa3d566e95e553c3fd4838e8d3cb722742
Nikita Sobol <mykyta.sobol@nure.ua>
2018-09-05 11:02:05 +0300

    LoginComponent.vue routes deleted

--- frontend/src/components/LoginComponent.vue
+++ frontend/src/components/LoginComponent.vue
@@ -68,26 +68,3 @@
    background-color: #ffa000;
  }
 </style>
-import Vue from 'vue';
-import Router from 'vue-router';
-import Home from './views/Home.vue';
-import Login from './views/Login.vue';
-
-Vue.use(Router);
-
-export default new Router({
-   mode: 'history',
-   base: process.env.BASE_URL,
-   routes: [
-       {
-           path: '/',
-           name: 'home',
-           component: Home,
-       },
-       {
-           path: '/login',
-           name: 'login',
-           component: Login,
-       },
-   ],
-});
