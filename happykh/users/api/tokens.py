3d5f1c3c7534bb1dc7efd2dc8454f99a06c25edc
vlad <vlad-shev@ukr.net>
2018-09-11 16:39:16 +0300

    added docstrings in tokens file

--- happykh/users/api/tokens.py
+++ happykh/users/api/tokens.py
@@ -1,9 +1,17 @@
+""" Token generators module"""
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.utils import six
 
 
 class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
+    """ Email confirmation token """
     def _make_hash_value(self, user, timestamp):
+        """
+        Make hash for token
+        :param user: user object
+        :param timestamp: timestamp
+        :return: hash
+        """
         return six.text_type(user.pk) + six.text_type(timestamp)
 
 
