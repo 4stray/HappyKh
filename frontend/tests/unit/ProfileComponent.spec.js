b050a0057b0b2bc6ce6042bb3c1397c016855f49
4stray <thebestmemberboy@gmail.com>
2018-09-12 11:24:10 +0300

    added test to check mounted component

--- frontend/tests/unit/ProfileComponent.spec.js
+++ frontend/tests/unit/ProfileComponent.spec.js
@@ -4,9 +4,7 @@ import ProfileComponent from '../../src/components/ProfileComponent.vue';
 const expect = require('chai').expect;
 const should = require('chai').should();
 
-describe('ProfileComonent', () => {
-  const wrapper = shallowMount(ProfileComponent);
-
+describe('ProfileComponent data() attributes check', () => {
   it('has userFirstName', () => {
     expect(ProfileComponent.data()).to.have.property('userFirstName');
   });
@@ -27,8 +25,39 @@ describe('ProfileComonent', () => {
     expect(ProfileComponent.data().isDisabled).to.be.equal(true);
   });
 
-  it('has male gender initially', () => {
-    expect(ProfileComponent.data().userGender).to.be.equal('Male');
+  it('has button to enable edit suited text initially', () => {
+    expect(ProfileComponent.data().enableText).to.be.equal('Enable editing');
   });
 
+  it('has userGender', () => {
+    expect(ProfileComponent.data()).to.have.property('userGender');
+  });
+
+  it('has userImage', () => {
+    expect(ProfileComponent.data()).to.have.property('userImage');
+  });
+});
+
+describe('ProfileComponent mounted check on fields', () => {
+  const wrapper = shallowMount(ProfileComponent);
+
+  it('contains LastName field', () => {
+    expect(wrapper.contains('#last_name')).to.be.equal(true);
+  });
+
+  it('contains FirstName field', () => {
+    expect(wrapper.contains('#first_name')).to.be.equal(true);
+  });
+
+  it('contains Age field', () => {
+    expect(wrapper.contains('#age')).to.be.equal(true);
+  });
+
+  it('contains Email field', () => {
+    expect(wrapper.contains('#email')).to.be.equal(true);
+  });
+
+  it('contains Image field', () => {
+    expect(wrapper.contains('#image')).to.be.equal(true);
+  })
 });
