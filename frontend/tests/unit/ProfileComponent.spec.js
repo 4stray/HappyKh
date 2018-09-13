7693bd76e0db0ec4b5bb8930ac5a6199e421d762
4stray <thebestmemberboy@gmail.com>
2018-09-13 10:52:37 +0300

    added interactions tests and set suitable description

--- frontend/tests/unit/ProfileComponent.spec.js
+++ frontend/tests/unit/ProfileComponent.spec.js
@@ -4,60 +4,75 @@ import ProfileComponent from '../../src/components/ProfileComponent.vue';
 const expect = require('chai').expect;
 const should = require('chai').should();
 
-describe('ProfileComponent data() attributes check', () => {
+describe('ProfileComponent data() check', () => {
   it('has userFirstName', () => {
     expect(ProfileComponent.data()).to.have.property('userFirstName');
   });
 
-  it('has userLastName', () => {
+  it('has default userLastName', () => {
     expect(ProfileComponent.data()).to.have.property('userLastName');
   });
 
-  it('has userAge', () => {
+  it('has default userAge', () => {
     expect(ProfileComponent.data()).to.have.property('userAge');
   });
 
-  it('has userEmail', () => {
+  it('has default userEmail', () => {
     expect(ProfileComponent.data()).to.have.property('userEmail');
   });
 
-  it('image change disabled initially', () => {
+  it('fields edit disabled initially', () => {
     expect(ProfileComponent.data().isDisabled).to.be.equal(true);
   });
 
-  it('has button to enable edit suited text initially', () => {
+  it('has default enableText', () => {
     expect(ProfileComponent.data().enableText).to.be.equal('Enable editing');
   });
 
-  it('has userGender', () => {
+  it('has default userGender', () => {
     expect(ProfileComponent.data()).to.have.property('userGender');
   });
 
-  it('has userImage', () => {
+  it('has default userImage', () => {
     expect(ProfileComponent.data()).to.have.property('userImage');
   });
 });
 
-describe('ProfileComponent mounted check on fields', () => {
+describe('ProfileComponent after mounted check fields', () => {
   const wrapper = shallowMount(ProfileComponent);
 
-  it('contains LastName field', () => {
-    expect(wrapper.contains('#last_name')).to.be.equal(true);
+  it('has LastName field with "text" type', () => {
+    expect(wrapper.find('#last_name').attributes('type')).to.be.equal('text');
   });
 
-  it('contains FirstName field', () => {
-    expect(wrapper.contains('#first_name')).to.be.equal(true);
+  it('has FirstName field with "text" type', () => {
+    expect(wrapper.find('#first_name').attributes('type')).to.be.equal('text');
   });
 
-  it('contains Age field', () => {
-    expect(wrapper.contains('#age')).to.be.equal(true);
+  it('has Age field with "number" type', () => {
+    expect(wrapper.find('#age').attributes('type')).to.be.equal('number');
   });
 
-  it('contains Email field', () => {
-    expect(wrapper.contains('#email')).to.be.equal(true);
+  it('has Email field with "text" type', () => {
+    expect(wrapper.find('#email').attributes('type')).to.be.equal('text');
   });
 
-  it('contains Image field', () => {
-    expect(wrapper.contains('#image')).to.be.equal(true);
-  })
+  it('contains Image from data()', () => {
+    expect(wrapper.find('#image').attributes('src')).to.be.equal(ProfileComponent.data().userImage);
+  });
+});
+
+describe('ProfileComponent interactions', () => {
+  const wrapper = shallowMount(ProfileComponent);
+  const btn = wrapper.find('.btn-change');
+  btn.trigger('click');
+
+  it('has interaction to enable fields on edit', () => {
+    const email = wrapper.find('#email');
+    expect(email.attributes('disabled')).to.be.equal(undefined);
+  });
+
+  it('has interaction to change buttons text', () => {
+    expect(btn.text()).to.be.equal('Disable editing');
+  });
 });
