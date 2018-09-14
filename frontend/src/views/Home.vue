7f98f084476df6ec55eadfec3d3af55c38a01cf4
SofiiaOmelchenko <sofiia.omelchenko@nure.ua>
2018-09-14 09:46:07 +0300

    delete file, reformated code

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,13 +6,13 @@
 </template>
 
 <script>
-// @ is an alias to /src
-import HomeComponent from '@/components/HomeComponent.vue';
+  // @ is an alias to /src
+  import HomeComponent from '@/components/HomeComponent.vue';
 
-export default {
-  name: 'home',
-  components: {
-    HomeComponent,
-  },
-};
+  export default {
+    name: 'home',
+    components: {
+      HomeComponent,
+    },
+  };
 </script>
