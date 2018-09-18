300b12eb0d8a289378705f30d5e37fb6fcf4f006
yavrsky <yavrsky@gmail.com>
2018-09-18 21:21:20 +0300

    tests were rewritten on the latest updates

--- frontend/tests/unit/RegistrationComponent.spec.js
+++ frontend/tests/unit/RegistrationComponent.spec.js
@@ -19,18 +19,25 @@ describe('RegistrationComponent', () => {
     expect(RegistrationComponent.data()).to.have.property('userPassword');
   });
 
-  it('contains username input field', () => {
-    expect(wrapper.contains('[name="username"]')).to.be.equal(true);
+  it('contains userEmail input field', () => {
+    expect(wrapper.contains('[type="email"]')).to.be.equal(true);
+    expect(wrapper.contains('[placeholder="EMAIL"]')).to.be.equal(true);
   });
+
   it('contains password input field', () => {
-    expect(wrapper.contains('[name="password"]')).to.be.equal(true);
+    expect(wrapper.contains('[type="password"]')).to.be.equal(true);
+    expect(wrapper.contains('[placeholder="PASSWORD"]')).to.be.equal(true);
   });
 
   it('contains confirmPassword input field', () => {
-    expect(wrapper.contains('[name="confirmPassword"]')).to.be.equal(true);
+    expect(wrapper.contains('[type="password"]')).to.be.equal(true);
+    expect(wrapper.contains('[placeholder="CONFIRM PASSWORD"]')).to.be.equal(true);
   });
 
   it('contains button for submitting user credentials', () => {
-    expect(wrapper.contains('[id="btn-registration"]')).to.be.equal(true);
+    expect(wrapper.contains('[class="btn-submit"]')).to.be.equal(true);
+    expect(wrapper.contains('[type="submit"]')).to.be.equal(true);
+    expect(wrapper.contains('[value="REGISTER"]')).to.be.equal(true);
+
   });
 });
