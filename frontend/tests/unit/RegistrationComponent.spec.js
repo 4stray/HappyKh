4dd2a2d0826937c9d3e79d4a6a16a4311bd40ae6
yavrsky <yavrsky@gmail.com>
2018-09-26 16:30:29 +0300

    refactor and bugfixes

--- frontend/tests/unit/RegistrationComponent.spec.js
+++ frontend/tests/unit/RegistrationComponent.spec.js
@@ -4,59 +4,110 @@ import RegistrationComponent from '@/components/RegistrationComponent.vue';
 const expect = require('chai').expect;
 const should = require('chai').should();
 
-describe('RegistrationComponent data', () => {
-  it('has default userEmail', () => {
-    expect(RegistrationComponent.data()).to.have.property('userEmail', '');
-  });
 
-  it('has default userPassword', () => {
-    expect(RegistrationComponent.data()).to.have.property('userPassword', '');
-  });
+describe('RegistrationComponent check for vue instance', () => {
+  const wrapper = shallowMount(RegistrationComponent);
 
-  it('has default confirmPassword', () => {
-    expect(RegistrationComponent.data()).to.have.property('confirmPassword', '');
-  });
-
-  it('has defalut errors', () => {
-    expect(RegistrationComponent.data()).to.have.deep.property('errors', { email: '', password: [] });
+  it('is a vue instance', () => {
+    expect(wrapper.isVueInstance()).to.be.equal(true);
   });
 });
 
 
-describe('RegistrationComponent', () => {
+describe('RegistrationComponent data', () => {
+  const data = RegistrationComponent.data();
+
+  it('has default userEmail', () => {
+    expect(data).to.have.property('userEmail', '');
+  });
+
+  it('has default userPassword', () => {
+    expect(data).to.have.property('userPassword', '');
+  });
+
+  it('has default confirmPassword', () => {
+    expect(data).to.have.property('confirmPassword', '');
+  });
+
+  it('has defalut errors', () => {
+    expect(data).to.have.deep.property('errors', {
+      email: '',
+      password: []
+    });
+  });
+});
+
+
+describe('RegistrationComponent contains the corrcet values', () => {
   const wrapper = shallowMount(RegistrationComponent);
 
   it('is a vue instance', () => {
     expect(wrapper.isVueInstance()).to.be.equal(true);
   });
 
-  it('has default userEmail', () => {
-    expect(RegistrationComponent.data()).to.have.property('userEmail');
-  });
-
-  it('has default userPassword', () => {
-    expect(RegistrationComponent.data()).to.have.property('userPassword');
-  });
-
   it('contains userEmail input field', () => {
-    expect(wrapper.contains('[type="email"]')).to.be.equal(true);
-    expect(wrapper.contains('[placeholder="EMAIL"]')).to.be.equal(true);
+    expect(wrapper
+        .find('#userEmail')
+        .attributes('type'))
+      .to.be.equal('email');
   });
 
-  it('contains password input field', () => {
-    expect(wrapper.contains('[type="password"]')).to.be.equal(true);
-    expect(wrapper.contains('[placeholder="PASSWORD"]')).to.be.equal(true);
+  it('contains userEmail placeholder suitable field attribute', () => {
+    expect(wrapper
+        .find('#userEmail')
+        .attributes('placeholder'))
+      .to.be.equal('EMAIL');
   });
 
-  it('contains confirmPassword input field', () => {
-    expect(wrapper.contains('[type="password"]')).to.be.equal(true);
-    expect(wrapper.contains('[placeholder="CONFIRM PASSWORD"]')).to.be.equal(true);
+  it('contains userPassword input field', () => {
+    expect(wrapper
+        .find('#userPassword')
+        .attributes('type'))
+      .to.be.equal('password');
+  });
+
+  it('contains userPassword placeholder suitable field attribute', () => {
+    expect(wrapper
+        .find('#userPassword')
+        .attributes('placeholder'))
+      .to.be.equal('PASSWORD');
+  });
+
+  it('contains userPassword input field', () => {
+    expect(wrapper
+        .find('#confirmPassword')
+        .attributes('type'))
+      .to.be.equal('password');
+  });
+
+  it('contains userPassword placeholder suitable field attribute', () => {
+    expect(wrapper
+        .find('#confirmPassword')
+        .attributes('placeholder'))
+      .to.be.equal('CONFIRM PASSWORD');
   });
 
   it('contains button for submitting user credentials', () => {
-    expect(wrapper.contains('[class="btn-submit"]')).to.be.equal(true);
-    expect(wrapper.contains('[type="submit"]')).to.be.equal(true);
-    expect(wrapper.contains('[value="REGISTER"]')).to.be.equal(true);
-    // expect(wrapper.find('#value')).attributes('REGISTER')
+    expect(wrapper
+        .find('.btn-submit')
+        .attributes('type'))
+      .to.be.equal('submit');
+  });
+
+  it('contains button with certain value', () => {
+    expect(wrapper
+        .find('.btn-submit')
+        .attributes('value'))
+      .to.be.equal('REGISTER');
+  });
+});
+
+
+describe('RegistrationComponent interactions', () => {
+  const wrapper = shallowMount(RegistrationComponent);
+  const btn = wrapper.find('.btn-submit');
+
+  it('button is not active if conditions are not met', () => {
+    expect(btn.attributes('disabled')).to.be.equal('disabled');
   });
 });
