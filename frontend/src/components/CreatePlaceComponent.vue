0d359e07faa2eb11524e17c06623045747d25e3f
vlad <vlad-shev@ukr.net>
2018-09-30 22:53:37 +0300

    add create place component

---
+++ frontend/src/components/CreatePlaceComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="createPlaceComponent">
+
+  </div>
+</template>
+
+<script>
+  export default {
+      name: 'createPlaceComponent',
+      data(){},
+      methods:{}, 
+  }
+</script>
+
+<style>
+
+</style>
