1891f1a740a3231f0deac1d33434cd4957d5b206
SofiiaOmelchenko <sofiia.omelchenko@nure.ua>
2018-10-01 13:36:11 +0300

    Moved Sign Out link to right

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,23 +8,12 @@
   @import '~vue-awesome-notifications/dist/styles/style.scss';
 
   #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    font-family: 'Liberation Sans', sans, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
+    width: 100%;
   }
 
-  #nav {
-    padding: 30px;
-  }
-
-  #nav a {
-    font-weight: bold;
-    color: #2c3e50;
-  }
-
-  #nav a.router-link-exact-active {
-    color: #42b983;
-  }
 </style>
--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,59 +1,82 @@
 <template>
-  <div id="nav">
+  <nav id="nav">
     <router-link :to="{ name: 'home'}">Home</router-link>
-    |
-    <router-link v-if="isAuthenticated" :to="{ name: 'profile'}">Profile</router-link>
-    <p v-if="isAuthenticated"> | </p>
-    <router-link v-if="!isAuthenticated" :to="{ name: 'login'}">Login</router-link>
-    <router-link v-if="isAuthenticated"
-                 v-on:click.native="signOut()"
-                 to="{name: login}"
-                 replace>Sign out</router-link>
-  </div>
+    <router-link v-if="isAuthenticated" :to="{ name: 'profile'}">Profile
+    </router-link>
+    <router-link v-if="!isAuthenticated" :to="{ name: 'login'}">Login
+    </router-link>
+    <div id="left-nav">
+      <router-link
+          v-if="isAuthenticated"
+          v-on:click.native="signOut()"
+          :to="{name: 'login'}"
+          replace>Sign out
+      </router-link>
+    </div>
+  </nav>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+  import {mapGetters} from 'vuex';
 
-export default {
-  name: 'Header',
-  computed: {
-    ...mapGetters({
-      isAuthenticated: 'getAuthenticated',
-    }),
-  },
-  methods: {
-    signOut() {
-      this.$cookies.remove('token');
-      this.$cookies.remove('user_id');
-      this.$router.push({ name: 'home' });
-      if (document.location.pathname === '/') {
-        document.location.reload(true);
-      }
+  export default {
+    name: 'Header',
+    computed: {
+      ...mapGetters({
+        isAuthenticated: 'getAuthenticated',
+      }),
     },
-  },
-};
+    methods: {
+      signOut() {
+        this.$cookies.remove('token');
+        this.$cookies.remove('user_id');
+        this.$router.push({name: 'home'});
+        if (document.location.pathname === '/') {
+          document.location.reload(true);
+        }
+      },
+    },
+  };
 </script>
-<style>
+<style scoped lang="scss">
   #nav {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    padding: 30px;
-  }
+    height: 50px;
+    /*position: fixed;*/
+    /*top: 0;*/
+    /*left: 0;*/
+    width: 100%;
 
-  #nav a {
-    font-weight: bold;
-    color: #2c3e50;
-  }
+    box-shadow: 0 2px 4px -1px rgba(0, 0, 0, 0.2), 0 4px 5px 0 rgba(0, 0, 0, 0.14), 0 1px 10px 0 rgba(0, 0, 0, 0.12);
+    -webkit-box-shadow: 0 2px 4px -1px rgba(0, 0, 0, 0.2), 0 4px 5px 0 rgba(0, 0, 0, 0.14), 0 1px 10px 0 rgba(0, 0, 0, 0.12);
+    -moz-box-shadow: 0 2px 4px -1px rgba(0, 0, 0, 0.2), 0 4px 5px 0 rgba(0, 0, 0, 0.14), 0 1px 10px 0 rgba(0, 0, 0, 0.12);
 
-  #nav a.router-link-exact-active {
-    color: #42b983;
-  }
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
 
-  #nav p {
-    display: inline;
+    #left-nav {
+      flex: 1 1 auto;
+      display: flex;
+      justify-content: flex-end;
+    }
+
+    a {
+      font-weight: bold;
+      font-size: 20px;
+      color: #2c3e50;
+      text-decoration: none;
+      display: inline-block;
+      padding: 0 20px;
+
+      &.router-link-exact-active {
+        color: #42b983;
+      }
+      &:hover {
+        color: #42b983;
+      }
+    }
+
   }
 </style>
