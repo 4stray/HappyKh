01e5726e8875dac9e1d1cd6877d650c4767b5588
Nikita Sobol <mykyta.sobol@nure.ua>
2018-10-02 15:17:09 +0300

    deleted reduntant styles from HomeComp, aligned styles in PlacesComp

--- frontend/src/components/HomeComponent.vue
+++ frontend/src/components/HomeComponent.vue
@@ -1,8 +1,6 @@
 <template>
-  <div>
-    <div class="HomeComponent">
-      <h1>{{ msg }}</h1>
-    </div>
+  <div class="HomeComponent">
+    <h1>{{ msg }}</h1>
     <PlacesComponent v-if="isAuthenticated"/>
   </div>
 </template>
@@ -30,22 +28,6 @@ export default {
 </script>
 
 <style scoped lang="scss">
-  h3 {
-    margin: 40px 0 0;
-  }
 
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-
-  a {
-    color: #42b983;
-  }
 </style>
 
--- frontend/src/components/PlaceComponent.vue
+++ frontend/src/components/PlaceComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="place-body">
     <div class="place-image">
-      <img v-bind:src=place.logo alt="uploading picture failed">
+      <img v-bind:src=place.logo alt="Uploading picture has failed">
     </div>
 
     <div>
@@ -20,15 +20,15 @@ export default {
 </script>
 
 <style scoped>
-  .place-body {
-    width: 40%;
-    border: 1px solid #CCCCCC;
-    background-color: #FFFFFF;
-    box-shadow: 3px 3px 2px grey;
-    margin: 20px;
-    display: inline-block;
-  }
-  .place-image {
-    height: 60px;
-  }
+.place-body {
+  width: 40%;
+  border: 1px solid #CCCCCC;
+  background-color: #FFFFFF;
+  box-shadow: 3px 3px 2px grey;
+  margin: 20px;
+  display: inline-block;
+}
+.place-image {
+  height: 60px;
+}
 </style>
