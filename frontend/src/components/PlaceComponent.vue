4468b877cb415f08e42d6af5875e1618824d63f0
Nikita Sobol <mykyta.sobol@nure.ua>
2018-10-02 11:55:36 +0300

    added connection between places and place components

--- frontend/src/components/PlaceComponent.vue
+++ frontend/src/components/PlaceComponent.vue
@@ -1,16 +1,32 @@
 <template>
-  <div>
-    <!--<img class="logo">-->
-    <p class="name"></p>
+  <div class="place-body">
+    <div class="place-image">
+      <img v-on:src=place.logo alt="Blank">
+    </div>
+    <div>
+      <p>Here {{ place.name }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'PlaceComponent'
+  name: 'PlaceComponent',
+  props: [
+    'place',
+  ],
 }
 </script>
 
 <style scoped>
-
+  .place-body {
+    width: 25%;
+    border: 1px solid #CCCCCC;
+    background-color: #FFFFEE;
+    box-shadow: 3px 3px 2px grey;
+    margin: 0 20px;
+  }
+  .place-image {
+    height: 60px;
+  }
 </style>
--- frontend/src/components/PlacesComponent.vue
+++ frontend/src/components/PlacesComponent.vue
@@ -1,21 +1,33 @@
 <template>
-  <div>
-    <button>Add Place</button>
-    <div class="places-body">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-      Adipisci aspernatur atque blanditiis eaque exercitationem facere
-      fugiat libero modi neque officia pariatur quibusdam recusandae sequi
-      totam veritatis vero, voluptatibus. Ab, quidem!
-    </div>
+  <div class="places-body">
+      <div class="add-place-button">
+        <router-link :to="{ name: 'createPlace' }">
+          Add Place
+        </router-link>
+      </div>
+
+      <div>
+        <PlaceComponent v-for="place in allPlaces"
+                        v-bind:place="place"/>
+      </div>
   </div>
 </template>
 
 <script>
 import Authentication from './Authentication/auth';
+import PlaceComponent from './PlaceComponent';
 import axios from 'axios';
 
 export default {
   name: 'PlacesComponent',
+  components: {
+    PlaceComponent,
+  },
+  data() {
+    return {
+      allPlaces: [],
+    };
+  },
   created() {
     const apiUrl = 'http://localhost:8000/api/places/';
     const header = {
@@ -23,14 +35,24 @@ export default {
         Authorization: Authentication.getAuthenticationHeader(this)
       },
     };
-    axios.get(apiUrl, {
-      headers,
-    }).then((response) => {
-      // ??
-    }).error((error) => {
-      console.log(error);
-    })
-    // axios.get()
+    this.allPlaces = [
+      {
+        name: 'First',
+      },
+      {
+        name: 'Second',
+      },
+      {
+        name: 'Third',
+      },
+    ]
+    // axios.get(apiUrl, {
+    //   apiUrl,
+    // }).then((response) => {
+    //   this.allPlaces = response.data
+    // }).error((error) => {
+    //   console.log(error);
+    //});
   }
 }
 </script>
@@ -38,6 +60,13 @@ export default {
 <style scoped>
 .places-body {
   margin: 0 auto;
+  padding: 0 auto;
   width: 60%;
 }
+.add-place-button {
+  width: 90px;
+  margin-left: auto;
+  margin-right: 0px;
+}
 </style>
+
