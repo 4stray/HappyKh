df279d7808839756d6fb73ba891559aaaeecfaa4
daria <daryashapovalova7@gmail.com>
2018-10-03 13:37:54 +0300

    create place api

---
+++ happykh/places/api/urls.py
@@ -0,0 +1,11 @@
+"""Urls to api's views for app places"""
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [ #pylint: disable = invalid-name
+    path('places/', include([
+         path('', views.PlacePage.as_view()),
+         ])),
+    ]
+
