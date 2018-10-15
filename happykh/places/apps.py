6894a7ab4bb60502dc7718f5736576ea65e4e8a8
yavrsky <yavrsky@gmail.com>
2018-10-15 16:29:21 +0300

    added pylint-django

--- happykh/places/apps.py
+++ happykh/places/apps.py
@@ -1,7 +1,7 @@
-"""Crates app place for django"""
-
+"""Module for registration app"""
 from django.apps import AppConfig
 
 
 class PlacesConfig(AppConfig):
+    """Class for places app registration"""
     name = 'places'
