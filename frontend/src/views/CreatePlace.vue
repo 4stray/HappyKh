c1c48724adea2bc507788812bea5b516a4577f11
SofiiaOmelchenko <sofiia.omelchenko@nure.ua>
2018-10-15 13:49:12 +0300

    fixed favicon

--- frontend/src/views/CreatePlace.vue
+++ frontend/src/views/CreatePlace.vue
@@ -1,20 +1,16 @@
 <template>
   <div class="CreatePlace">
-    <Header/>
     <CreatePlaceComponent/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import Header from '../components/Header.vue';
 import CreatePlaceComponent from '../components/CreatePlaceComponent.vue';
 
 export default {
   name: 'CreatePlace',
   components: {
     CreatePlaceComponent,
-    Header,
   },
 };
 </script>
