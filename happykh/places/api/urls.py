10a1a91a651ac065fe0f42ba0b1cc83ac1e43bf1
4stray <thebestmemberboy@gmail.com>
2018-10-16 15:58:11 +0300

    added singleplace view and get request handler within

--- happykh/places/api/urls.py
+++ happykh/places/api/urls.py
@@ -7,5 +7,6 @@ from . import views
 urlpatterns = [ #pylint: disable = invalid-name
     path('places/', include([
          path('', views.PlacePage.as_view()),
+         path('<int:id>', views.PlaceSinglePage.as_view()),
          ])),
     ]
--- happykh/places/api/views.py
+++ happykh/places/api/views.py
@@ -1,3 +1,4 @@
+import logging
 from django.contrib.sites.shortcuts import get_current_site
 from rest_framework import status
 from rest_framework.response import Response
@@ -7,6 +8,8 @@ from rest_framework.permissions import IsAuthenticated
 
 from .serializers import PlaceSerializer
 from ..models import Place
+
+LOGGER = logging.getLogger('happy_logger')
 
 
 class PlacePage(APIView):
@@ -35,3 +38,28 @@ class PlacePage(APIView):
             return Response({'message': 'place was created'},
                             status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PlaceSinglePage(APIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    variation = Place.large
+
+    def get(self, request, id):
+
+        try:
+            single_place = Place.objects.get(pk=id)
+
+        except Place.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        else:
+            context = {
+                'variation': self.variation,
+                'domain': get_current_site(request)
+            }
+            serializer = PlaceSerializer(single_place, context=context)
+            LOGGER.info(f'Requested place with id: {id}')
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
