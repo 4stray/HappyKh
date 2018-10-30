07059235bbd824995a1e39987d34ab6dd09c032a
4stray <thebestmemberboy@gmail.com>
2018-10-30 17:07:50 +0200

    removed pad function, added docstrings

--- happykh/users/cryptography.py
+++ happykh/users/cryptography.py
@@ -2,22 +2,17 @@ from Crypto.Cipher import DES
 from happykh.settings import ENCODING_DES_KEY
 
 
-def pad(text):
-    while len(text) % 8 != 0:
-        text += b' '
-    return text
-
-
 def encode(encoding_text):
     """
-
+    Encodes str value into bytes using DES, returns it's hex representation
     :param encoding_text: String
     :return:
     """
     des = DES.new(ENCODING_DES_KEY.encode(), DES.MODE_ECB)
 
-    padded_text = pad(encoding_text.encode())
-    encoded_text = des.encrypt(padded_text)
+    while (len(encoding_text) % 8) != 0:
+        encoding_text += " "
+    encoded_text = des.encrypt(encoding_text.encode())
     hex_representation = encoded_text.hex()
 
     return hex_representation
@@ -25,9 +20,9 @@ def encode(encoding_text):
 
 def decode(decoding_hex):
     """
-
-    :param decoding_hex:
-    :return:
+    Decodes bytes' hex representation into string using DES
+    :param decoding_hex: String
+    :return: String
     """
     des = DES.new(ENCODING_DES_KEY.encode(), DES.MODE_ECB)
 
