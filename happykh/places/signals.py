f5ba8ca54e657fb05255e5a1c2b44c64c75b6ed1
Dmitri Roganin <dmitriroganin@Dmitris-MacBook-Pro.local>
2018-11-05 15:53:34 +0200

    added doc strings for signals

--- happykh/places/signals.py
+++ happykh/places/signals.py
@@ -10,7 +10,15 @@ LOGGER = logging.getLogger('happy_logger')
 
 
 @receiver(pre_delete, sender=Place)
-def delete_media_files(sender, instance, using, **kwargs):
+def delete_media_files(sender, instance, **kwargs):
+    """
+    Function for deleting media files of Place, before deleting instance.
+
+    :param sender: class, instances of which we are deleting
+    :param instance: object, whose files we are deleting
+    :param kwargs: external parameters
+    :return: None
+    """
     LOGGER.info('Deleted files of place in signal.')
     delete_std_images_from_media(instance.logo,
                                  sender.VARIATIONS_LOGO)
