a910356298b11541a514e089f89a99efde73e292
vlad <vlad-shev@ukr.net>
2018-11-07 17:02:08 +0200

    delete dublicate code

--- happykh/users/api/views.py
+++ happykh/users/api/views.py
@@ -422,9 +422,6 @@ class UserPassword(APIView):
                 return Response({'message': 'Wrong password'},
                                 status=status.HTTP_400_BAD_REQUEST)
 
-            token_key = request.META['HTTP_AUTHORIZATION'][6:]
-            Token.objects.get(key=token_key).delete()
-
             serializer.update(user, serializer.data)
 
             token_key = request.META['HTTP_AUTHORIZATION'][6:]
