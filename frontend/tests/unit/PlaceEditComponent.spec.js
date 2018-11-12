7d182d2e4b4a23b449db0cd12be0d9559f47b653
Nikita Sobol <mykyta.sobol@nure.ua>
2018-11-12 23:40:10 +0200

    run linter

--- frontend/tests/unit/PlaceEditComponent.spec.js
+++ frontend/tests/unit/PlaceEditComponent.spec.js
@@ -1,9 +1,10 @@
 import VueRouter from 'vue-router';
-import {createLocalVue, shallowMount} from '@vue/test-utils';
+import { createLocalVue, shallowMount } from '@vue/test-utils';
 import PlaceEditComponent from '@/components/PlaceEditComponent.vue';
 
 
 const expect = require('chai').expect;
+
 const localVue = createLocalVue();
 const router = new VueRouter();
 
