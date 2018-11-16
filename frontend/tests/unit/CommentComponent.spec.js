eea70ad83a5ad130f94fdc25963890bbfcd183e4
vlad <vlad-shev@ukr.net>
2018-11-16 17:14:52 +0200

    fix front comment tests

--- frontend/tests/unit/CommentComponent.spec.js
+++ frontend/tests/unit/CommentComponent.spec.js
@@ -1,4 +1,4 @@
-import { config, shallowMount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import CommentComponent from '../../src/components/CommentComponent.vue';
 
 const expect = require('chai').expect;
@@ -17,4 +17,17 @@ describe('CommentComponent', () => {
   it('contains element with author`s full name', () => {
     expect(wrapper.contains('.fullName')).to.be.equal(true);
   });
+
+  it('contains control panel', () => {
+    expect(wrapper.contains('.commentControl')).to.be.equal(true);
+  });
+
+  it('contains control commentUpdate button', () => {
+    expect(wrapper.contains('.commentEdit')).to.be.equal(true);
+  });
+
+  it('contains control commentDelete button', () => {
+    expect(wrapper.contains('.commentDelete')).to.be.equal(true);
+  });
+
 });
