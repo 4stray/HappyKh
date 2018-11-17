572db7e25eb201a8733189ccee530ca921dbb9da
Dmitri Roganin <dmitriroganin@Dmitris-MacBook-Pro.local>
2018-11-17 19:45:49 +0200

    refactored test_login

--- backend/tests/users/test_login.py
+++ backend/tests/users/test_login.py
@@ -14,6 +14,11 @@ LOGIN_URL = '/api/users/login'
 CORRECT_DATA = {'user_email': CORRECT_EMAIL,
                 'user_password': CORRECT_PASSWORD}
 
+ACTIVATED_USER_EMAIL = 'active@gmail.com'
+ACTIVATED_USER_PASSWORD = 'activePassword'
+ACTIVATED_USER_DATA = {'user_email': ACTIVATED_USER_EMAIL,
+                       'user_password': ACTIVATED_USER_PASSWORD}
+
 
 class LoginViewTestCase(BaseTestCase, APITestCase):
     """Test user api login view /api/users/login/"""
@@ -22,6 +27,11 @@ class LoginViewTestCase(BaseTestCase, APITestCase):
         """Create user objects"""
         self.test_user = User.objects.create_user(email=CORRECT_EMAIL,
                                                   password=CORRECT_PASSWORD)
+        self.active_user = User.objects.create_user(
+            email=ACTIVATED_USER_EMAIL,
+            password=ACTIVATED_USER_PASSWORD,
+            is_active=True
+        )
 
     def test_invalid_email(self):
         """Test view response for invalid email"""
@@ -63,29 +73,15 @@ class LoginViewTestCase(BaseTestCase, APITestCase):
         If user's token in database expired, he can still login successfully
         and token in database will be changed for a new one.
         """
-        activated_email = 'active@gmail.com'
-        activated_password = 'activePassword'
-        active_user = User.objects.create_user(email=activated_email,
-                                 password=activated_password,
-                                 is_active=True)
-        active_data = {'user_email': activated_email,
-                       'user_password': activated_password}
-        token = Token.objects.create(user=active_user)
+        token = Token.objects.create(user=self.active_user)
         token.created = timezone.now() - timezone.timedelta(
-            days=token_life_duration*2)
+            days=token_life_duration * 2)
         token.save()
-        response = self.client.post(LOGIN_URL, active_data)
+        response = self.client.post(LOGIN_URL, ACTIVATED_USER_DATA)
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(0, Token.objects.filter(key=token.key).count())
 
     def test_successful_response(self):
         """Test view response for correct data"""
-        activated_email = 'active@gmail.com'
-        activated_password = 'activePassword'
-        User.objects.create_user(email=activated_email,
-                                 password=activated_password,
-                                 is_active=True)
-        active_data = {'user_email': activated_email,
-                       'user_password': activated_password}
-        response = self.client.post(LOGIN_URL, active_data)
+        response = self.client.post(LOGIN_URL, ACTIVATED_USER_DATA)
         self.assertEqual(status.HTTP_200_OK, response.status_code)
